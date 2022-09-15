cbbc4556babeb4bbbe9813163a23bede29529f59
Oleg Mitorun <68237469+mitorun@users.noreply.github.com>
2022-09-16 03:03:21 +0700

    Add files via upload

---
+++ fls/phpmailer/mail-exp-2.php
@@ -0,0 +1,74 @@
+<?php
+// Это экспериментальный файл от Болдырева 03.09.2022, тут добавлена строка $body = ""; и удален повторяющийся тайтл.
+
+// Файлы phpmailer:
+require 'PHPMailer.php';
+require 'SMTP.php';
+require 'Exception.php';
+
+//$title = "Тема письма";
+$file = $_FILES['file'];
+
+$c = true;
+
+// Формирование самого письма:
+$title = "Сообщение из Mitorun Studio";
+$body = "";
+foreach ( $_POST as $key => $value ) {
+	if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+		$body .= "
+		" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+			<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+			<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+		</tr>
+		";
+	}
+}
+
+$body = "<table style='width: 100%;'>$body</table>";
+
+// Настройки PHPMailer:
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+	$mail->isSMTP();
+	$mail->CharSet = "UTF-8";
+	$mail->SMTPAuth   = true;
+
+	// Настройки вашей почты:
+	$mail->Host       = 'smtp.gmail.com'; /* SMTP сервера вашей почты. Яндекс: smtp.yandex.ru ssl 465 Мейл.ру: smtp.mail.ru ssl 465 */
+	$mail->Username   = 'bearru@gmail.com'; /* Логин на почте my@example.com */
+	$mail->Password   = 'zzpklurylczpgsdw'; /* Пароль ПРИЛОЖЕНИЯ на почте (его нужно получить-запросить специально) */
+	$mail->SMTPSecure = 'ssl';
+	$mail->Port       = 465;
+
+	// Адрес и имя отправителя:
+	$mail->setFrom('bearru@gmail.com', 'Заявка с Mitorun Studio');
+
+	// Заполнить емейл куда будут приходить письма:
+	$mail->addAddress('contact@mitorun.studio');
+	//$mail->addAddress('mitorun@mail.ru');
+
+	// Прикрипление файлов к письму:
+	if (!empty($file['name'][0])) {
+		for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
+			$uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
+			$filename = $file['name'][$ct];
+			if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
+					$mail->addAttachment($uploadfile, $filename);
+					$rfile[] = "Файл $filename прикреплён";
+			} else {
+					$rfile[] = "Не удалось прикрепить файл $filename";
+			}
+		}
+	}
+
+	// Отправка сообщения:
+	$mail->isHTML(true);
+	$mail->Subject = $title;
+	$mail->Body = $body;
+	$mail->send();
+
+} catch (Exception $e) {
+	$status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
